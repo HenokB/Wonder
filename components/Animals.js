f882313b0c4792c4a2debd3576b262db4cb6f20f
Henok B Ademtew <henokb2124@gmail.com>
2022-08-19 00:21:52 +0300

    some updates

--- components/Animals.js
+++ components/Animals.js
@@ -1,17 +1,20 @@
 import styles from '../styles/Home.module.css'
 import Image from 'next/image'
-import animal from '../public/bg.jpg'
+import animal from '../public/animal.jpg'
 
 
 export function Animals(){
     return(
         <div className={styles.animalPage}>
-            <h1>Animal Page</h1>
+            {/* <h1>Animal Page</h1> */}
             <p className={styles.p}>
-            A Simple Typing Effect with Blinking Cursor. This is a simple yet beautiful 
-            typewriter effect created using CSSA Simple Typing Effect with Blinking Cursor. 
-            This is a simple yet beautiful typewriter effect created using CSS
-            </p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+            pariatur. Excepteur sint occaecat cupidatat non proident, 
+            sunt in culpa qui officia deserunt mollit anim id est laborum            </p>
             <Image
                 src={animal}
                 alt="a picture of animals" 
--- components/Navbar.js
+++ components/Navbar.js
@@ -5,15 +5,18 @@ import {Animals} from '../components/Animals'
 import styles from '../styles/Home.module.css'
 export const Navbar = () => {
   return (
-    <div class={styles.container}>
-    <h1>Wonder</h1>
+    <div className={styles.container}>
+    <h1>Wonder🌍</h1>
     <Image
         src={hero}
         alt="A random picture of a forest"
         
     />
-    <button className={styles.btn}>Button</button>
-    <h1  className={styles.h12}>Wonder</h1>
+    <button className={styles.btn}>Telegram</button>
+    {/* <h1  className={styles.h12}>Wonder</h1> */}
+    <div class={styles.typewriter}>
+        <h1>Wonder</h1>
+    </div>
     </div>
 
 
